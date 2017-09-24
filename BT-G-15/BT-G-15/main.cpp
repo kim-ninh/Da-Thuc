7d41bbe8c8a57f2e632174fdc18de8f61d364ab3
kimninh <1612484@student.hcmus.edu.vn>
2017-09-24 11:31:55 +0700

    Tạo 1 số bug thử nghiệm

--- BT-G-15/BT-G-15/main.cpp
+++ BT-G-15/BT-G-15/main.cpp
@@ -1,5 +1,31 @@
-#include<iostream>      
+#include<iostream> 
 using namespace std;
+
+int TinhTong(int n)
+{
+	int tong; 
+	for (int i = 1; i <= n; i++)
+		tong += i;
+	return tong;
+}
+
+int TinhGiaiThua(int n)
+{
+	int GiaiThua;
+	for (int i = 1; i <= n; i++)
+		GiaiThua *= i;
+	return GiaiThua;
+}
+
+int DemSoNguyenTo(int a[], int n)
+{
+	int dem;
+	for (int i = 0; i < n; i++)
+		if (LaSoNguyenTo(a[i]) == true)
+			dem++;
+	return dem;
+}
+
 int main()
 {
 	cout << "Hello World";
