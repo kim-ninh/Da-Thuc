955d5eda81465b61bbe68eb34e5b860165414dff
kimninh <1612484@student.hcmus.edu.vn>
2017-11-12 21:57:37 +0700

    Cập nhập lại DaThuc.cpp

--- BT-G-15/BT-G-15/DaThuc.cpp
+++ BT-G-15/BT-G-15/DaThuc.cpp
@@ -1,1 +1,38 @@
 #include "DaThuc.h"
+DaThuc::DaThuc()
+{
+	donthuc->data.hs = 1;
+	donthuc->data.bien->ten = 'a';
+	donthuc->data.bien->bac = 1;
+
+	donthuc->next->data.hs = 1;
+	donthuc->next->data.bien->ten = 'b';
+	donthuc->next->data.bien->bac = 1;
+	donthuc->next->next = NULL;
+}
+DaThuc::~DaThuc()
+{
+	while (donthuc != NULL)
+	{
+		delete[] donthuc;
+		donthuc = donthuc->next;
+	}
+}
+ostream& operator <<(ostream & os, NodeDonThuc donthuc)
+{
+	os << donthuc.data.hs << "*" << donthuc.data.bien->ten << "^" << donthuc.data.bien->bac;
+	return os;
+}
+void DaThuc::InRaFile(fstream fileOut)
+{
+	fileOut.open("DaThuc.txt", ios_base::out);
+	while (donthuc->next != NULL)
+	{
+		fileOut << donthuc;
+		if (donthuc->data.hs > 0)
+			fileOut << '+';
+		donthuc = donthuc->next;
+	}
+
+	fileOut.close();
+}
